2adc304b211c8c7ce67a6589cef090c63b859bce
putrihandayani2 <putrihandayani997@gmail.com>
2021-09-03 07:29:12 +0800

    Add files via upload

---
+++ DTS_LAB 2021/2.4.1.7 Variables.py
@@ -0,0 +1,9 @@
+john = 3
+mary = 5
+adam = 6
+
+print("Adam's apples :", john, "\nMary's apples :", mary, "\nAdam's apples :", adam)
+
+total_apples = john + mary + adam
+
+print("They had", total_apples, "apples")
