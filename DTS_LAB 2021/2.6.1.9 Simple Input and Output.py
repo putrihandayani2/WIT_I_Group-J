5b6040b617a12304514b0285b7f0c34a987de03c
putrihandayani2 <putrihandayani997@gmail.com>
2021-09-03 14:07:41 +0800

    Add files via upload

---
+++ DTS_LAB 2021/2.6.1.9 Simple Input and Output.py
@@ -0,0 +1,9 @@
+a = float(input("Input value a :")) # input a float value for variable a here
+b = float(input("Input value b :"))# input a float value for variable b here
+
+print("a + b =", a+b)# output the result of addition here
+print("a - b =", a-b)# output the result of subtraction here
+print("a * b =", a*b)# output the result of multiplication here
+print("a / b =", a/b)# output the result of division here
+
+print("\nThat's all, folks!")
